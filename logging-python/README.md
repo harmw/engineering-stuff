f9e934a8286021d74fe30d172a5092779a7ca73e
Harm Weites <harm@weites.com>
2024-08-20 20:12:42 +0200

    chore: refactor to a more true cli using click

--- logging-python/README.md
+++ logging-python/README.md
@@ -5,5 +5,21 @@ This implements the [logging bits](../logging) using `python`.
 To generate the example log file please use generate.sh from the above location.
 
 ```bash
-python3 go.py
+pip3 install -r requirements.txt
+```
+
+```bash
+python3 go.py reader -f httpd.log
+```
+
+Example output:
+```
+{
+    "192.0.2.42": 104,
+    "192.0.2.99": 27,
+    "192.0.2.26": 2,
+    "192.0.2.27": 2,
+    "192.0.2.72": 2,
+    "192.0.2.81": 2,
+[..]
 ```
--- logging-python/go.py
+++ logging-python/go.py
@@ -1,33 +1,50 @@
 #!/usr/bin/env python3
+import click
+import sys
 import re
 import json
 
 
-LOG = "httpd.log"
+@click.group()
+def cli():
+    """Simple log reader"""
+    pass
 
-# example:
-# [Tue Aug 20 19:31:56 CEST 2024] 192.0.2.42 /foo5 GET Mozilla/5.0 (something something user-agent)
 
-pattern = r"\[(.*?)\] (\d+\.\d+\.\d+\.\d+) (\S+) (\S+) (.*)"
+@cli.command()
+@click.option("-f", help="name of the file to read", required=True)
+def reader(f):
+    """Reads the logfile and counts hits per ip"""
 
-hits = {}
+    # example:
+    # [Tue Aug 20 19:31:56 CEST 2024] 192.0.2.42 /foo5 GET Mozilla/5.0 (something something user-agent)
 
-try:
-    with open(LOG, "r") as f:
-        lines = f.readlines()
-        for line in lines:
-            match = re.match(pattern, line.strip())
-            if match:
-                ip = match.group(2)
-                if ip in hits:
-                    hits[ip] += 1
-                else:
-                    hits[ip] = 1
-except Exception as e:
-    print(f"oops: {e}")
-    raise
+    pattern = r"\[(.*?)\] (\d+\.\d+\.\d+\.\d+) (\S+) (\S+) (.*)"
 
-data = {k: v for k, v in sorted(hits.items(), key=lambda item: item[1], reverse=True)}
+    hits = {}
 
-pretty = json.dumps(data, indent=4)
-print(pretty)
+    try:
+        with open(f, "r") as log_file:
+            lines = log_file.readlines()
+            for line in lines:
+                match = re.match(pattern, line.strip())
+                if match:
+                    ip = match.group(2)
+                    if ip in hits:
+                        hits[ip] += 1
+                    else:
+                        hits[ip] = 1
+    except Exception as e:
+        click.secho(f"oops: {e}", fg="red")
+        sys.exit()
+
+    data = {
+        k: v for k, v in sorted(hits.items(), key=lambda item: item[1], reverse=True)
+    }
+
+    pretty = json.dumps(data, indent=4)
+    print(pretty)
+
+
+if __name__ == "__main__":
+    cli()
